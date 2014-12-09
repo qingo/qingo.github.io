4ef862d63fbe242bb2e0cb5079704f14f389f9c6
Qingo <97383102@qq.com>
2014-12-09 18:12:31 +0800

    blog

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-    blog = require('gulp-blog');
+    blog = require('gulp-blog'),
+    rename = require('gulp-rename');
 
 gulp.task('page', function () {
   gulp.src('./src/page/**/*.md')
       .pipe(blog())
+      .pipe(rename({extname: ".html"}))
       .pipe(gulp.dest('www'))
 });

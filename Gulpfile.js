7ecbb8ffee8d9fba1e2441655e66b80d004b26ff
Qingo <97383102@qq.com>
2015-01-05 17:23:58 +0800

    Understanding design pattern

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,13 @@
 var gulp = require('gulp'),
     blog = require('gulp-blog'),
     md = require('gulp-markdown'),
-    header = require('gulp-header'),
-    footer = require('gulp-footer'),
-    fs = require('fs'),
-    through = require('through2');
+    fix = require('gulp-fix'),
+    fs = require('fs');
 
 gulp.task('page', function () {
   gulp.src('./src/page/**/*.md')
-      .pipe(md({
-      }))
+      .pipe(md())
       .pipe(blog())
-      .pipe(header(fs.readFileSync('./config/qingo.prefix', 'utf8')))
-      .pipe(footer(fs.readFileSync('./config/qingo.suffix', 'utf8')))
+      .pipe(fix(fs.readFileSync('./config/qingo.prefix', 'utf8'), fs.readFileSync('./config/qingo.suffix', 'utf8'), null, null, ['json']))
       .pipe(gulp.dest('www'))
 });
